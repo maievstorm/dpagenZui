2d0e60420a676bd782f7471373977dc080cd5eed
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-06-25 13:44:56 +0700

    Update MonitorJob.jsx

--- src/views/dataingest/MonitorJob.jsx
+++ src/views/dataingest/MonitorJob.jsx
@@ -1,64 +1,211 @@
-import React  from "react";
 import { useState, useEffect } from 'react'   
 import axios from 'axios'; // npm instal axios
-import {IconPlayerPlay} from '@tabler/icons';
 import config from "../../config";
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import { useNavigate } from "react-router-dom"; 
-import Paper from '@material-ui/core/Paper'; 
-import { makeStyles } from '@material-ui/core/styles'; 
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
- 
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
 
-import {
-    Table,
-    TableHead,
-    TableCell,
-    TableBody,
-    TableRow,
-    TablePagination,
-    TableContainer
-} from '@mui/material'
- 
-
-
-const useStyles = makeStyles({  
- 
-      root: {  
-        width: '100%',  
-      },  
-        container: {  
-        maxHeight: '100%',  
-        },  
-    
-    });
-    const icons = {
-      IconPlayerPlay
-    };   
    
- 
- 
+    function descendingComparator(a, b, orderBy) {
+      if (b[orderBy] < a[orderBy]) {
+        return -1;
+      }
+      if (b[orderBy] > a[orderBy]) {
+        return 1;
+      }
+      return 0;
+    }
+    
+    function getComparator(order, orderBy) {
+      return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+    }
 
-export default function MonitorJob() {  
-    const classes = useStyles();  
-    const [page, setPage] = React.useState(0);  
-    const [data, setData] = useState([]);   
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);  
-    
-    
+    function stableSort(array, comparator) {
+      const stabilizedThis = array.map((el, index) => [el, index]);
+      stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+          return order;
+        }
+        return a[1] - b[1];
+      });
+      return stabilizedThis.map((el) => el[0]);
+    }
 
-    const handleChangePage = (event, newPage) => {  
-        setPage(newPage);  
-      };  
-    
-      const handleChangeRowsPerPage = event => {  
-        setRowsPerPage(+event.target.value);  
-        setPage(0);  
-      }; 
-      
+    const headCells = [
+      {
+        id: 'id',
+        numeric: true,
+        disablePadding: true,
+        label: 'id',
+      },
+      {
+        id: 'item_name',
+        numeric: false,
+        disablePadding: false,
+        label: 'Tên tiến trình',
+      },
+      {
+        id: 'customer_invoice_data',
+        numeric: false,
+        disablePadding: false,
+        label: 'Chi tiết tiến trình',
+      } 
+    ];
+
+ 
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Danh sách tiến trình
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <PlayCircleOutlineIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+export default function MonitorJob() {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setData] = useState([]);   
+
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
     const getairflowapi= config.rootapi+'/invoice/subntype/1&airflow';
  
     useEffect(() => {   
@@ -67,76 +214,146 @@ export default function MonitorJob() {
             setData(res.data.data);    
           })  
         },[] ); 
-        const [anchorEl, setAnchorEl] = React.useState(null);
-        const open = Boolean(anchorEl);
-        const handleClick = (event) => {
-          setAnchorEl(event.currentTarget);
-        };
-        const handleClose = () => {
-          setAnchorEl(null);
-        };    
 
-        const navigate = useNavigate()
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.item_name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
 
-        const onClickHandler = () => navigate('/dataingest/createflowjob')
-        
-    return (  
-      
-      <Paper className={classes.root}> 
-      <Box>
-      
-        <Button  onClick={onClickHandler} >Tạo tiến trình</Button>
-        <Button >Chỉnh sửa tiến trình</Button>
-        <Button>Xoá tiến trình</Button>
-       
-      
-      </Box>
-      
+  const handleClick = (event, item_name) => {
+    const selectedIndex = selected.indexOf(item_name);
+    let newSelected = [];
 
-       
-        <TableContainer className={classes.container}>  
-          <Table stickyHeader aria-label="sticky table">  
-          <TableHead>  
-              <TableRow>  
-                <TableCell>id</TableCell>  
-                <TableCell >Tên tiến trình</TableCell>  
-                {/* <TableCell >Chi tiết tiến trình</TableCell>   */}
-                <TableCell >Action</TableCell> 
-              </TableRow>  
-            </TableHead>  
-            <TableBody>  
-              {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {  
-                return (  
-  
-             <TableRow >  
-                  <TableCell component="th" scope="row">{row.id}</TableCell>  
-                  <TableCell >{row.item_name}</TableCell>  
-                  {/* <TableCell >{row.customer_invoice_data}</TableCell>   */}
-                  <TableCell ><icons.IconPlayerPlay/></TableCell>  
-                  
-                </TableRow>  
-                );  
-  
-              })}  
-            </TableBody>  
-          </Table>  
-        </TableContainer>  
-        <TablePagination
-                sx={{ px: 2 }}
-                rowsPerPageOptions={[10, 20, 30]}
-                component="div"
-                count={data?.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                backIconButtonProps={{
-                    'aria-label': 'Previous Page',
-                }}
-                nextIconButtonProps={{
-                    'aria-label': 'Next Page',
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, item_name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  const navigate = useNavigate()
+
+  const onClickHandler = () => navigate('/dataingest/createflowjob')
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+      <Button  onClick={onClickHandler} >Tạo tiến trình</Button>
+      
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
             />
-      </Paper>  
-    );  
-  } 
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.item_name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.item_name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.item_name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.id}
+                      </TableCell>
+                      <TableCell align="left">{row.item_name}</TableCell>
+                      <TableCell align="left">{row.customer_invoice_data}</TableCell>
+             
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
+}
