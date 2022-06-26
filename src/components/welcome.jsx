92dac7d0a5f976d5b87a10bab7c1fdbb26627962
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-06-26 23:14:02 +0700

    Update welcome.jsx

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,38 +1,33 @@
 import UserService from "../services/UserService";
 import React from 'react';
 import { AppBar,Toolbar,Button,CssBaseline,Typography } from "@mui/material";
- 
- 
- 
-
-
 import Logo from 'ui-component/Logo';
-const useStyles =() => ({
-    root: {
-        boxShadow: " 5px 5px #2196F3",
-        backgroundColor: "white",
-        border: '1px solid' 
-      } ,
+// const useStyles =() => ({
+//     root: {
+//         boxShadow: " 5px 5px #2196F3",
+//         backgroundColor: "white",
+//         border: '1px solid' 
+//       } ,
       
-    navlinks: {
-      marginLeft: 10,
-      display: "flex",
-    },
-   logo: {
-      flexGrow: "1",
-//       cursor: "pointer",
-      },
-    link: {
-      //textDecoration: "none",
-      color: "black",
-      fontSize: "15px",
-      marginLeft: 10,
-      "&:hover": {
-        color: "black",
-        borderBottom: "1px solid white",
-      },
-    },
-  });
+//     navlinks: {
+//       marginLeft: 10,
+//       display: "flex",
+//     },
+//    logo: {
+//       flexGrow: "1",
+// //       cursor: "pointer",
+//       },
+//     link: {
+//       //textDecoration: "none",
+//       color: "black",
+//       fontSize: "15px",
+//       marginLeft: 10,
+//       "&:hover": {
+//         color: "black",
+//         borderBottom: "1px solid white",
+//       },
+//     },
+//   });
  
 
 // export default class Welcome extends React.Component {
@@ -50,29 +45,29 @@ const useStyles =() => ({
 
 
 function Welcome() {
-    const classes = useStyles();
+    // const classes = useStyles();
   
     return (
       <AppBar position="static"  style={{backgroundColor : 'white'}}>
         <CssBaseline />
         <Toolbar>
-          <Typography variant="h4" className={classes.logo}>
+          <Typography variant="h4" >
             <Logo/>
           </Typography>
-            <div className={classes.navlinks}>
-              <Button to="/about" className={classes.link}>
+            <div >
+              <Button to="/about" >
                 Về chúng tôi
               </Button>
-              <Button  className={classes.link}>
+              <Button  >
                 Sản phẩm
               </Button>
-              <Button  className={classes.link}>
+              <Button  >
                 Liên hệ
               </Button>
-              <Button className={classes.link}>
+              <Button >
                 Đăng ký
               </Button>
-              <Button  className={classes.link }  onClick={() => UserService.doLogin()}>Đăng nhập</Button>
+              <Button  onClick={() => UserService.doLogin()}>Đăng nhập</Button>
             </div>
         </Toolbar>
       </AppBar>
