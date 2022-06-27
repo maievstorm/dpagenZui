12727765022f9a25e3f2ed9874384afbdcb37bb9
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-06-27 22:50:08 +0700

    Update UploadFilesService.jsx

--- src/views/objectstore/UploadFilesService.jsx
+++ src/views/objectstore/UploadFilesService.jsx
@@ -36,27 +36,3 @@ class UploadFilesService {
 
 export default new UploadFilesService();
 
-
-
-//   import http from "../http-common";
-
-// class UploadFilesService {
-//   upload(file, onUploadProgress) {
-//     let formData = new FormData();
-
-//     formData.append("file", file);
-
-//     return http.post("/upload", formData, {
-//       headers: {
-//         "Content-Type": "multipart/form-data",
-//       },
-//       onUploadProgress,
-//     });
-//   }
-
-//   getFiles() {
-//     return http.get("/files");
-//   }
-// }
-
-// export default new UploadFilesService();
