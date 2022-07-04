3e4c7615a5ec323d06069d1850c8a02e1a3e3e94
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-07-04 18:50:34 +0700

    Update index.js

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,7 +1,7 @@
 import dashboard from './dashboard';
 import pages from './pages';
 import utilities from './utilities';
-import other from './other';
+//import other from './other';
 import registerService from './registerService';
 
 
