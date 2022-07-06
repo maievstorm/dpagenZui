d7e7fd45e2c0408353cfd0d0c343ed6d0675c121
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-07-06 23:49:19 +0700

    Update RenderOnAuthenticated.jsx

--- src/components/RenderOnAuthenticated.jsx
+++ src/components/RenderOnAuthenticated.jsx
@@ -2,7 +2,7 @@ import UserService from "../services/UserService";
 
 const RenderOnAuthenticated = ({ children }) => (UserService.isLoggedIn()) ? children: null;
 
-console.log(UserService.isLoggedIn())
+
  
 
 export default RenderOnAuthenticated
